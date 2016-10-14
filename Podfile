8f27be1059af18e2223fcf9bebfc67118b8c725b
kris-randen <kris.randen@gmail.com>
2016-10-13 22:27:26 -0400

    Facebook Software Development Login Kit FBSDKLoginKit pod installed.

--- Podfile
+++ Podfile
@@ -10,5 +10,7 @@ pod 'Firebase/Auth'
 pod 'Firebase/Database'
 pod 'Firebase/Storage'
 
+pod 'FBSDKLoginKit'
+
 end
 
--- Pods/Target Support Files/Pods-SocialApp/Pods-SocialApp-frameworks.sh
+++ Pods/Target Support Files/Pods-SocialApp/Pods-SocialApp-frameworks.sh
@@ -82,3 +82,14 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-SocialApp/Bolts.framework"
+  install_framework "Pods-SocialApp/FBSDKCoreKit.framework"
+  install_framework "Pods-SocialApp/FBSDKLoginKit.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-SocialApp/Bolts.framework"
+  install_framework "Pods-SocialApp/FBSDKCoreKit.framework"
+  install_framework "Pods-SocialApp/FBSDKLoginKit.framework"
+fi
