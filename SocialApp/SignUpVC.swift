db6d464cf7d4d732c33a1cf2cc5593ee320b1407
kris-randen <kris.randen@gmail.com>
2016-10-15 21:43:41 -0400

    Created the Email and Password Log In so that we can test the App with multiple users and coaches. Eventually this needs to go away leaving only Facebook Login. But Sign Up requires indicating if the user is a user or a coach.

---
+++ SocialApp/SignUpVC.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpVC.swift
+//  SocialApp
+//
+//  Created by Kris Rajendren on Oct/15/16.
+//  Copyright © 2016 Campus Coach. All rights reserved.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
